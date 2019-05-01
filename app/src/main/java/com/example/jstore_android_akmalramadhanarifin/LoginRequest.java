29b270ad89291e77adc564eceae1a8df59003808
akmal28 <akmal.ramadhan28@gmail.com>
2019-05-01 20:53:38 +0700

    Modul 9 TP

---
+++ app/src/main/java/com/example/jstore_android_akmalramadhanarifin/LoginRequest.java
@@ -0,0 +1,23 @@
+package com.example.jstore_android_akmalramadhanarifin;
+
+import com.android.volley.Response;
+import com.android.volley.toolbox.StringRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginRequest extends StringRequest {
+    private static final String Login_URL = "http://192.168.43.199:8080/logincust";
+    private Map<String, String> params;
+
+    public LoginRequest(String email, String password, Response.Listener<String> listener){
+        super(Method.POST, Login_URL, listener, null);
+        params = new HashMap<>();
+        params.put("email", email);
+        params.put("password", password);
+    }
+
+    public Map<String, String> getParams(){
+        return params;
+    }
+}
